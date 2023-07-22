012a978afd8f589284080b3e62e6a71c0de636c5
David Dean <dave.dean@gmail.com>
2023-07-22 22:15:26 +1000

    add logout button, make nav react

--- react/hello-world/src/components/loginform.js
+++ react/hello-world/src/components/loginform.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ContainerTable from './containers';
+import Nav from './nav';
+
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -10,8 +12,33 @@ function App() {
     setIsAuthenticated(true);
   };
 
+
+  const handleLogout = () => {
+    delete axios.defaults.headers.common["Authorization"];
+    setIsAuthenticated(false);
+  };
+
+
+  useEffect(() => {
+    const interceptor = axios.interceptors.response.use(
+      response => response, 
+      error => {
+        if (error.response.status === 403) {
+          handleLogout();
+        }
+        return Promise.reject(error);
+      }
+    );
+    
+    return () => {
+      axios.interceptors.response.eject(interceptor);
+    };
+  }, []);
+
+
   return (
     <div className="App">
+        <Nav isAuthenticated={isAuthenticated} onLogout={handleLogout} />
       {isAuthenticated ? <ContainerTable /> : <LoginForm onLogin={handleLogin} />}
     </div>
   );
