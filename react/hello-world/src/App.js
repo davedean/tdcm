579a284cbd27540a2a6f1cf00c753daa2fe02086
David Dean <dave.dean@gmail.com>
2023-07-22 20:21:44 +1000

    temp commit

--- react/hello-world/src/App.js
+++ react/hello-world/src/App.js
@@ -1,23 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-/*
-const Ping = () => { useEffect(() => {
-  const [posts, setPosts] = useState([]);
-
-  fetch('http://localhost:8089/ping=10')
-     .then((response) => response.json())
-     .then((data) => {
-        console.log(data);
-        setPosts(data);
-     })
-     .catch((err) => {
-        console.log(err.message);
-     });
-}, []);
-}
-*/
-
 function App() {
   return (
     <div className="App">
--- react/hello-world/src/components/hello.js
+++ react/hello-world/src/components/hello.js
@@ -11,17 +11,25 @@ function Hello() {
 
 export default Hello;
 
+async function componentDidMount() {
 
-function componentDidMount() {
-    fetch("http://localhost:8089/v1/cameras")
-        .then((response) => response.json())
-    console.log("ffs")
-    alert('You clicked me!');
-    return (
-            <div>
-                <h2>pinged</h2>
-                <h2> {Response} </h2>
-            </div>
-    )
+  let response = await fetch("http://localhost:8089/ping")
+  let movies = await response.json();
+  console.log(movies);
+
+  if (response.ok) { // if HTTP-status is 200-299
+    // get the response body (the method explained below)
+    let json = await response.json();
+    console.log("lol"+json);
+  } else {
+    alert("HTTP-Error: " + response.status);
   }
+  
+  return (
+    <div>
+    <h2>pinged</h2>
+    <h2> {response} </h2>
+    </div>
+  )
+}
   
